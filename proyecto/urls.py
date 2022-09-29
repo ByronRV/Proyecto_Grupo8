91308dcdad0381f1a2ea19702ebbdb4551b136c8
Bayron Ramirez <norbydro@gmail.com>
2022-09-28 22:51:25 -0500

    Se soluciono problema de versiones

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
-from aplicacion import views
+from . import aplicacion
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
-    path('user/', views.UserCreateView.as_view()),
-    path('user/<int:pk>/', views.UserDetailView.as_view()),
+    path('user/', aplicacion.UserCreateView.as_view()),
+    path('user/<int:pk>/', aplicacion.UserDetailView.as_view()),
 ]
